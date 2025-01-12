a2f7a993d5f5ff0ea4efc23b0b7dbfe372ebff19
Danish <sheikhd@rknec.edu>
2025-01-12 11:53:50 +0530

    Added Set Inteface Collection Demos

---
+++ src/JavaCollectionFramework/Set/CopyOnWriteArraySetDemo.java
@@ -0,0 +1,40 @@
+package JavaCollectionFramework.Set;
+
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+public class CopyOnWriteArraySetDemo {
+    public static void main(String[] args) {
+        // Thread-Safe
+        // Copy-On-Write Mechanism
+        // No Duplicate Elements
+        // Iterators Do Not Reflect Modifications
+    	// stable photo
+
+        CopyOnWriteArraySet<Integer> copyOnWriteSet = new CopyOnWriteArraySet<>();
+        ConcurrentSkipListSet<Integer> concurrentSkipListSet = new ConcurrentSkipListSet<>();
+
+        for (int i = 1; i <= 5; i++) {
+            copyOnWriteSet.add(i);
+            concurrentSkipListSet.add(i);
+        }
+
+        System.out.println("Initial CopyOnWriteArraySet: " + copyOnWriteSet);
+        System.out.println("Initial ConcurrentSkipListSet: " + concurrentSkipListSet);
+// add after loop
+        System.out.println("\nIterating and modifying CopyOnWriteArraySet:");
+        for (Integer num : copyOnWriteSet) {
+            System.out.println("Reading from CopyOnWriteArraySet: " + num);
+            // Attempting to modify the set during iteration
+            copyOnWriteSet.add(6);
+        }
+// weakly consistent skiplist it may add or not at current
+        System.out.println("\nIterating and modifying ConcurrentSkipListSet:");
+        for (Integer num : concurrentSkipListSet) {
+            System.out.println("Reading from ConcurrentSkipListSet: " + num);
+            // Attempting to modify the set during iteration
+            concurrentSkipListSet.add(6);
+        }
+
+    }
+}
